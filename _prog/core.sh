6588aaa0b211e2bcbc332f8f701b48a1e230f8f5
mirage335 <spamfreemirage335@gmail.com>
2018-05-02 19:05:16 -0400

    WIP.

--- _prog/core.sh
+++ _prog/core.sh
@@ -1,2 +1,34 @@
 ##### Core
 
+_construct_generator_sequence() {
+	_start
+	
+	local localFunctionEntryPWD
+	localFunctionEntryPWD="$PWD"
+	
+	local generatorName
+	generatorName="$1"
+	local generatorTemplate
+	generatorTemplate=Python
+	
+	local generatorSource
+	generatorSource="$scriptAbsoluteFolder"/lib/blockly/generators/"$generatorTemplate"
+	local generatorDestination
+	generatorDestination="$scriptLocal"/templates/"$generatorName"
+	
+	mkdir "$generatorDestination"
+	
+	cp -a  "$generatorSource"/. "$generatorDestination"/
+	
+	
+	cd "$generatorDestination"/
+	
+	
+	#Patch.
+	
+	
+	
+	cd "$localFunctionEntryPWD"
+	_stop
+}
+
